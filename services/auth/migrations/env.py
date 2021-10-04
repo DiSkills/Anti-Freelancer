59ad558b1fb2923a736fa9c665a6728779de21cb
counter021 <ara123rus123ara@gmail.com>
2021-10-04 18:44:16 +0300

    Alembic fix

--- services/auth/migrations/env.py
+++ services/auth/migrations/env.py
@@ -8,6 +8,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 from app.models import User, Verification
+from db import Base
 
 config = context.config
 
@@ -19,7 +20,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [User.metadata, Verification.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
