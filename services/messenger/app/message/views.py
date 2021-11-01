004d05b8e62016abafea2c5d8fb336be3d52208b
counter021 <ara123rus123ara@gmail.com>
2021-11-01 13:18:36 +0300

    WS send

--- services/messenger/app/message/views.py
+++ services/messenger/app/message/views.py
@@ -1,6 +1,15 @@
 from fastapi import WebSocket
 
 
+async def websocket_error(websocket: WebSocket, detail: dict):
+    await websocket.send_json(
+        {
+            'type': 'ERROR',
+            'data': {'detail': detail}
+        }
+    )
+
+
 class WebSocketState:
     def __init__(self):
         self._websockets: dict[int, list[WebSocket]] = {}
@@ -12,10 +21,32 @@ class WebSocketState:
 
     async def leave(self, user_id: int, websocket: WebSocket):
         if user_id not in self._websockets.keys():
-            pass
+            await websocket_error(websocket, {'msg': 'User is not in state'})
+            return
 
         for index in range(len(self._websockets[user_id])):
             if self._websockets[user_id][index] == websocket:
                 await self._websockets[user_id][index].close()
                 del self._websockets[user_id][index]
                 break
+
+    async def send(self, sender_id: int, recipient_id: int, success_msg: str, response_type: str, data: dict):
+        for socket in self._websockets[sender_id]:
+            await socket.send_json(
+                {
+                    'type': 'SUCCESS',
+                    'data': {'msg': success_msg},
+                }
+            )
+
+        sockets = self._websockets[sender_id]
+        if recipient_id in self._websockets.keys():
+            sockets += self._websockets[recipient_id]
+
+        for socket in sockets:
+            await socket.send_json(
+                {
+                    'type': response_type,
+                    'data': data,
+                }
+            )
