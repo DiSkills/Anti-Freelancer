fc0720bf0658f0a1eaed73a5c50630332b70c43b
counter021 <ara123rus123ara@gmail.com>
2021-11-01 12:32:43 +0300

    Init

---
+++ services/messenger/README.md
@@ -0,0 +1,14 @@
+# Messenger Service
+
+## TODO
+
+- [ ] Docker
+- [ ] Logger errors in file
+- [ ] Permission requests
+    - [ ] Is authenticated
+    - [ ] Is active
+    - [ ] Is superuser
+    - [ ] Is customer
+    - [ ] Is freelancer
+- [ ] Message
+- [ ] Tests
---
+++ services/messenger/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from config import PROJECT_NAME
+
+app = FastAPI(
+    title=PROJECT_NAME,
+    version='0.2.0',
+    description='Messenger Service Anti-Freelancer by Counter021',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
