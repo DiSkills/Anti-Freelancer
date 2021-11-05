a19d5bc5d6e467c252e1ad64a7384a48fea3f7bd
counter021 <ara123rus123ara@gmail.com>
2021-11-05 15:47:54 +0300

    Get user dialogues

--- services/messenger/app/dialogues/routers.py
+++ services/messenger/app/dialogues/routers.py
@@ -16,7 +16,7 @@ dialogues_router = APIRouter()
     response_description='Dialogues',
     status_code=status.HTTP_200_OK,
     response_model=list[GetDialogue],
-    tags=['messages'],
+    tags=['dialogues'],
 )
 async def get_all_dialogues_for_user(user_id: int = Depends(is_active), db: AsyncSession = Depends(get_db)):
     return await views.get_all_dialogues_for_user(db, user_id)
