5688468d514fb3ee382fa717eeed855ade7f5471
counter021 <ara123rus123ara@gmail.com>
2021-11-20 14:42:18 +0300

    Docs

--- services/other/app/review/views.py
+++ services/other/app/review/views.py
@@ -6,6 +6,18 @@ from app.review.schemas import CreateReview
 
 
 async def create_review(db: AsyncSession, user_id: int, schema: CreateReview) -> dict:
+    """
+        Create review
+        :param db: DB
+        :type db: AsyncSession
+        :param user_id: User ID
+        :type user_id: int
+        :param schema: New review data
+        :type schema: CreateReview
+        :return: New review
+        :rtype: dict
+        :raise HTTPException 400: Review exist
+    """
 
     if await review_crud.exist(db, user_id=user_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Review exist')
